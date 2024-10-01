a413ec22c2f9144e068712042e2e928ec37d219a
chai35217793 <149489960+chai35217793@users.noreply.github.com>
2024-10-01 07:20:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+# Start services for devgagan1
+devgagan1() {
+    gunicorn app:app &
+    python main.py
+}
+
+# Start services for devgagan2
+devgagan2() {
+    cp __init__.py src/devgagan/
+    cd src
+    python -m devgagan
+}
+
+# Uncomment the required service
+# devgagan1
+# devgagan2
